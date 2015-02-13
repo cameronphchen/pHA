fda1bb4fbee4808a1c94472bc67432d50bc1b523
Cameron Chen <CameronChenx@gmail.com>
2015-02-13 12:13:28 -0500

    change path data to data_old

--- code/run_exp_mysseg_loo.py
+++ code/run_exp_mysseg_loo.py
@@ -56,10 +56,10 @@ nseg       = nTR/2 - win_size
 
 # load experiment options
 # rondo options
-options = {'input_path'  : '/jukebox/ramadge/pohsuan/pHA/data/input/', \
-           'working_path': '/fastscratch/pohsuan/pHA/data/working/'+str(para['nTR'])+'TR/rand'+str(para['ranNum'])+'/',\
-           'data_path'   : '/jukebox/ramadge/pohsuan/pHA/data/working/'+str(para['nTR'])+'TR/',\
-           'output_path' : '/jukebox/ramadge/pohsuan/pHA/data/output/'+str(para['nTR'])+'TR/'}
+options = {'input_path'  : '/jukebox/ramadge/pohsuan/pHA/data_old/input/', \
+           'working_path': '/fastscratch/pohsuan/pHA/data_old/working/'+str(para['nTR'])+'TR/rand'+str(para['ranNum'])+'/',\
+           'data_path'   : '/jukebox/ramadge/pohsuan/pHA/data_old/working/'+str(para['nTR'])+'TR/',\
+           'output_path' : '/jukebox/ramadge/pohsuan/pHA/data_old/output/'+str(para['nTR'])+'TR/'}
 
 # local options
 #options = {'input_path'  : '/Volumes/ramadge/pohsuan/pHA/data/input/', \

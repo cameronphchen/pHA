214088101dff46bc84e8a5ac56fbcbc5ec0cee43
Cameron Chen <CameronChenx@gmail.com>
2015-03-07 16:08:04 -0500

    update mysseg_loo.sh for running batch of experiment

--- code/sh_script/mysseg_loo/mysseg_loo.sh
+++ code/sh_script/mysseg_loo/mysseg_loo.sh
@@ -9,7 +9,7 @@ nTR=3535
 winsize=9
 ln -s /jukebox/ramadge/pohsuan/pHA/code/run_exp.py run_exp.py
 chmod +x run_exp.py
-for loo in $(seq 13 17)
+for loo in 4 5 6 7 8 10 11 13 #$(seq 5 8)
 do
 
   #submit      run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 1st -w $winsize noalign 10 1300 --strfresh
@@ -19,7 +19,7 @@ do
   #submit_long run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 1st -w $winsize ha 10 1300 --strfresh
   #submit_long run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 2nd -w $winsize ha 10 1300 --strfresh
 
-    for nfeat in 10 50 100 500 1300
+    for nfeat in 1300 #10 50 100 500 1300
     do 
     #submit run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 1st -w $winsize ppca 10 $nfeat --strfresh
     #submit run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 2nd -w $winsize ppca 10 $nfeat --strfresh    
@@ -27,10 +27,10 @@ do
         do
             #submit      run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 1st -w $winsize pica 1 $nfeat -r $rand --strfresh
             #submit      run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 2nd -w $winsize pica 1 $nfeat -r $rand --strfresh
-            submit run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 1st -w $winsize ha_syn 10 $nfeat -r $rand --strfresh
-            submit run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 2nd -w $winsize ha_syn 10 $nfeat -r $rand --strfresh
-            #submit_long run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 1st -w $winsize pha_em 10 $nfeat -r $rand --strfresh
-            #submit_long run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 2nd -w $winsize pha_em 10 $nfeat -r $rand --strfresh
+            #submit run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 1st -w $winsize ha_syn 10 $nfeat -r $rand --strfresh
+            #submit run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 2nd -w $winsize ha_syn 10 $nfeat -r $rand --strfresh
+            pni_submit -q long.q -P long -l vf=20G run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 1st -w $winsize pha_em 10 $nfeat -r $rand --strfresh
+            pni_submit -q long.q -P long -l vf=20G run_exp.py $dataset $nvoxel $nTR mysseg --loo $loo -e 2nd -w $winsize pha_em 10 $nfeat -r $rand --strfresh
         done
     done
 done

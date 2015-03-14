d8713b096fb04f4aa5010b1264210873e9906c42
Cameron Chen <CameronChenx@gmail.com>
2015-03-14 10:53:12 -0400

    [ENH] increase list of alignment algorithms

--- code/plot/create_algo_list.py
+++ code/plot/create_algo_list.py
@@ -2,15 +2,7 @@ import pickle
 
 algo_list = []
 
-algo = {'name': 'noalign',
-  'align_algo': 'noalign',
-  'nfeature': '1300',
-  'kernel': None,
-  'rand': False
-}
-algo_list.append(algo)
-
-algo = {'name': 'HA',
+algo = {'name': 'HA (1300vx)',
   'align_algo': 'ha',
   'nfeature': '1300',
   'kernel': None,
@@ -18,9 +10,10 @@ algo = {'name': 'HA',
 }
 algo_list.append(algo)
 
+
 algo = {
-  'name': 'pHA 10',
-  'align_algo': 'pha_em',
+  'name': 'pHAc 10',
+  'align_algo': 'ha_syn',
   'nfeature': '10',
   'kernel': None,
   'rand': True
@@ -28,8 +21,8 @@ algo = {
 algo_list.append(algo)
 
 algo = {
-  'name': 'pHA 50',
-  'align_algo': 'pha_em',
+  'name': 'pHAc 50',
+  'align_algo': 'ha_syn',
   'nfeature': '50',
   'kernel': None,
   'rand': True
@@ -37,8 +30,8 @@ algo = {
 algo_list.append(algo)
 
 algo = {
-  'name': 'pHA 100',
-  'align_algo': 'pha_em',
+  'name': 'pHAc 100',
+  'align_algo': 'ha_syn',
   'nfeature': '100',
   'kernel': None,
   'rand': True
@@ -46,8 +39,8 @@ algo = {
 algo_list.append(algo)
 
 algo = {
-  'name': 'pHA 500',
-  'align_algo': 'pha_em',
+  'name': 'pHAc 500',
+  'align_algo': 'ha_syn',
   'nfeature': '500',
   'kernel': None,
   'rand': True
@@ -55,8 +48,8 @@ algo = {
 algo_list.append(algo)
 
 algo = {
-  'name': 'pHA 1300',
-  'align_algo': 'pha_em',
+  'name': 'pHAc 1300',
+  'align_algo': 'ha_syn',
   'nfeature': '1300',
   'kernel': None,
   'rand': True

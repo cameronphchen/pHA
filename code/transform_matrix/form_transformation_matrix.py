e5223f63fce9cf73113b2421c481474970d16b3f
Cameron Chen <CameronChenx@gmail.com>
2015-03-16 10:12:57 -0400

    incoporate spha_vi_tm into transform matrix

--- code/transform_matrix/form_transformation_matrix.py
+++ code/transform_matrix/form_transformation_matrix.py
@@ -10,7 +10,7 @@ def transform(args, workspace_lh, workspace_rh, nsubjs):
   if args.align_algo in ['ha','ha_syn','ha_syn_kernel']:
     transform_lh = workspace_lh['R']
     transform_rh = workspace_rh['R']
-  elif args.align_algo in ['pha_em','spha_vi', 'ppca_vert',  'pica_vert']:
+  elif args.align_algo in ['pha_em','spha_vi', 'spha_vi_tm', 'ppca_vert',  'pica_vert']:
     bW_lh = workspace_lh['bW']
     bW_rh = workspace_rh['bW']
     for m in range(nsubjs):

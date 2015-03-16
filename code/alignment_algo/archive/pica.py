0b16115c390f34a79564f72b04a7f71d7024458b
Cameron Chen <CameronChenx@gmail.com>
2015-03-16 14:21:22 -0400

    BF: fix pica to be consistent with factor analysis framework

--- code/alignment_algo/archive/pica.py
+++ code/alignment_algo/archive/pica.py
@@ -6,17 +6,8 @@
 #movie_data[:,:,m] is the data for subject m, which will be X_m^T in the standard 
 #mathematic notation
 
-# prior
-# bK_i  : TR x TR
+# do ICA on bX (nvoxel x nsubjs*nTR)
 
-# Variational Parameters:
-# mu_s  : nvoxel x TR
-# Sig_s : TR x TR x nvoxel
-
-# Hyperparameters:
-# W_m   : nvoxel x nfeature x nsubjs
-# rho2  : nsubjs 
-# mu    : nvoxel*nsubj 
 
 import numpy as np, scipy, random, sys, math, os
 from scipy import stats
@@ -24,33 +15,33 @@ import sys
 sys.path.append('/jukebox/ramadge/pohsuan/scikit-learn/sklearn')
 from sklearn.decomposition import FastICA
 
-def pICA(movie_data, options, para, lrh):
-  print 'pICA(scikit-learn)'
-  nvoxel = movie_data.shape[0]
-  nTR    = movie_data.shape[1]
-  nsubjs = movie_data.shape[2]
+def align(movie_data, options, args, lrh):
+    print 'pICA(scikit-learn)'
+    nvoxel = movie_data.shape[0]
+    nTR    = movie_data.shape[1]
+    nsubjs = movie_data.shape[2]
 
-  align_algo = para['align_algo']
-  nfeature   = para['nfeature']
-  
-  if not os.path.exists(options['working_path']):
-    os.makedirs(options['working_path'])
+    align_algo = args.align_algo
+    nfeature = args.nfeature
+    randseed = args.randseed
 
-  # zscore the data
-  bX = np.zeros((nsubjs*nTR,nvoxel))
-  for m in range(nsubjs):
-    for t in range(nTR):
-      bX[nTR*m+t,:] = stats.zscore(movie_data[:,t,m].T ,axis=0, ddof=1)
-  del movie_data
-  
-  ica = FastICA(n_components= nfeature, max_iter=500)
-  ica.fit(bX.T)
-  R = ica.transform(bX.T)
-  print bX.shape
-  print R.shape
+    # zscore the data
+    bX = np.nan((nsubjs*nvoxel, nTR))
+    for m in xrange(nsubjs):
+        bX[m*nvoxel:(m+1)*nvoxel, :] = stats.zscore(movie_data[:, :, m].T, axis=0, ddof=1).T
+    del movie_data
 
-  niter = 1  
-  # initialization when first time run the algorithm
-  np.savez_compressed(options['working_path']+align_algo+'_'+lrh+'_'+str(para['nvoxel'])+'vx_'+str(niter)+'.npz',\
-                                R = R,  niter=niter)
-  return niter
+    np.random.seed(randseed)
+    A = np.mat(np.random.random((nfeature, nfeature)))
+
+    ica = FastICA(n_components=nfeature, max_iter=500, w_init=A, random_state=randseed)
+    ica.fit(bX.T)
+    bW = ica.transform(bX.T)
+
+    print bW.shape
+
+    niter = 10
+    # initialization when first time run the algorithm
+    np.savez_compressed(options['working_path']+align_algo+'_'+lrh+'_'+str(niter)+'.npz',\
+                                  bW = bW,  niter=niter)
+    return niter

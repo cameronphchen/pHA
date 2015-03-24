935a2b52cd4a0e6aa4cee84615cb58ed39743acb
Cameron Chen <CameronChenx@gmail.com>
2015-03-24 10:41:28 -0400

    ENH: remove irrelevant code

--- code/preprocessing/forrest/aggregate_roi_data_distributed_to_one.py
+++ code/preprocessing/forrest/aggregate_roi_data_distributed_to_one.py
@@ -2,35 +2,18 @@
 import scipy.io
 import os,sys
 import numpy as np
+import sys
+sys.path.append('/jukebox/ramadge/pohsuan/PyMVPA-hyper/')
 import mvpa2
 from mvpa2.datasets.mri import fmri_dataset
 from scipy.signal import butter, lfilter
 
 
-roi = 'pt'
-template_path = '/jukebox/fastscratch/pohsuan/pHA/data/raw/forest/'+\
-                'psydata.ovgu.de/forrest_gump/templates/grpbold7Tad/from_mni/'
-mask_fname = os.path.join(template_path, roi+'_mask.nii.gz')
+roi = 'vt'
+
 nsubj = 20
 nrun  = 8
 nTR_remove = 4
-
-# Sample rate and desired cutoff frequencies (in Hz).
-fs = 1/2.0 #one sample every two seconds
-lowcut = 1/150.0
-highcut = 1/9.0 
-
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band')
-    return b, a
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
 
 forrest_movie_all = np.empty((nsubj,1), dtype=object)
 

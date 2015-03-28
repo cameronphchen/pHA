6ecad9145b25c01bf5337d966c777ff59d4e2ef2
Cameron Chen <CameronChenx@gmail.com>
2015-03-28 10:30:12 -0400

    working version idvclas with svm, doesn't have different feature for all and group

--- code/run_exp_noLR_idvclas.py
+++ code/run_exp_noLR_idvclas.py
@@ -36,7 +36,7 @@ parser.add_argument("nTR", type = int,
                     help="number of TRs in the dataset")
 
 parser.add_argument("exptype",    help="name of the experiment type")
-parser.add_argument("l", "--loo", type = int,
+parser.add_argument("-l", "--loo", type = int,
                     help="whether this experiment is loo experiment")
 parser.add_argument("-e","--expopt",    help="experiment options e.g. 1st or 2nd")
 parser.add_argument("-w", "--winsize", type = int,
@@ -48,7 +48,7 @@ parser.add_argument("-k", "--kernel", metavar='',
 
 parser.add_argument("niter"     , type = int,  
                     help="number of iterations to the algorithm")
-parser.add_argument("nfeature", type=int, 
+parser.add_argument("nfeature", type=int,
                     help="number of features")
 parser.add_argument("-r", "--randseed", type=int, metavar='',
                     help="random seed for initialization")
@@ -97,111 +97,154 @@ if not os.path.exists(options['working_path']):
 if args.strfresh:
   if os.path.exists(options['working_path']+args.align_algo+'__current.npz'):
     os.remove(options['working_path']+args.align_algo+'__current.npz')
+  if os.path.exists(options['working_path']+'group1/'+args.align_algo+'__current.npz'):
+    os.remove(options['working_path']+'group1/'+args.align_algo+'__current.npz')
+  if os.path.exists(options['working_path']+'group2/'+args.align_algo+'__current.npz'):
+    os.remove(options['working_path']+'group2/'+args.align_algo+'__current.npz')
 
 # terminate the experiment early if the experiment is already done
 #if os.path.exists(options['working_path']+args.align_algo+'_acc_10.npz'):
 #  sys.exit('experiment already finished, early termination')
 
-
 print 'start loading data'
 # load data for alignment and prediction
 # load movie data after voxel selection by matdata_preprocess.m
-if args.exptype == 'midvclas':
-  if args.loo != None:
-    sys.exit('no loo subject')
+if args.exptype in ['idvclas', 'idvclas_svm']:
+    if args.loo == None:
+        sys.exit('no loo subject')
 
-  movie_data = scipy.io.loadmat(options['input_path']+'movie_data.mat')
-  movie_data = movie_data['movie_data']
+    movie_data = scipy.io.loadmat(options['input_path']+'movie_data.mat')
+    movie_data = movie_data['movie_data']
 
-  align_data = np.zeros((movie_data.shape))
-  nsubj_group = movie_data.shape[2]/2 #assuming two groups, and they have equal amount of people
+    align_data = np.zeros((movie_data.shape))
+    nsubj_group = movie_data.shape[2]/2 #assuming two groups, and they have equal amount of people
 
-  for m in range(align_data.shape[2]):
-      align_data[:,:,m] = stats.zscore(movie_data[:,:,m].T ,axis=0, ddof=1).T
+    for m in range(align_data.shape[2]):
+        align_data[:,:,m] = stats.zscore(movie_data[:,:,m].T ,axis=0, ddof=1).T
 
-  # align all data to remove the shared part
-  opt_all_folder  = str(args.nfeature) + 'feat/' + \
-              ("rand"+str(args.randseed)+'/' if args.randseed != None else "identity/" )+ 'all/'
-  options_all = copy.deepcopy(options)
-  options_all['working_path'] = '/fastscratch/pohsuan/pHA/data/working/'+data_folder+exp_folder+alg_folder+opt_all_folder
+    # align all data to remove the shared part
+    opt_all_folder  = str(args.nfeature) + 'feat/' + \
+                ("rand"+str(args.randseed)+'/' if args.randseed != None else "identity/" )+ 'all/'
+    options_all = copy.deepcopy(options)
+    options_all['working_path'] = '/fastscratch/pohsuan/pHA/data/working/'+data_folder+exp_folder+alg_folder+opt_all_folder
 
-  if os.path.exists(options_all['working_path']+args.align_algo+'__10.npz'):
-      algo = importlib.import_module('alignment_algo.'+args.align_algo)
-      algo.align(align_data, options_all, args, '')
-  workspace = np.load(options_all['working_path']+args.align_algo+'__10.npz')
+    if not os.path.exists(options_all['working_path']):
+        os.mkdir(options_all['working_path'])
 
-  W = workspace['R']
-  S = workspace['G'].T
+    if not os.path.exists(options_all['working_path']+args.align_algo+'__10.npz'):
+        for iter in range(10):
+            if os.path.exists(options_all['working_path']+args.align_algo+'__10.npz'):
+                break
+            algo = importlib.import_module('alignment_algo.'+args.align_algo)
+            algo.align(align_data, options_all, args, '')
+    workspace = np.load(options_all['working_path']+args.align_algo+'__10.npz')
 
-  for m in range(align_data.shape[2]):
-      align_data[:,:,m] = align_data[:,:,m] - W[:,:,m].dot(S)
+    W = workspace['R']
+    S = workspace['G'].T
 
-  align_data_group1 = np.copy(align_data[:,:,:nsubj_group])
-  align_data_group2 = np.copy(align_data[:,:,nsubj_group:])
+    #for m in range(align_data.shape[2]):
+    #    tmp = align_data[:,:,m] - W[:,:,m].dot(S)
+    #    align_data[:,:,m] = stats.zscore(tmp.T ,axis=0, ddof=1).T
 
-  pred_data = np.copy(align_data[:,:,args.loo])
+    align_data_group1 = np.copy(align_data[:,:,:nsubj_group])
+    align_data_group2 = np.copy(align_data[:,:,nsubj_group:])
+
+    pred_data_group1 = np.copy(align_data_group1[:,:,args.loo])
+    pred_data_group2 = np.copy(align_data_group2[:,:,args.loo])
 else:
-  sys.exit('invalid experiment type')
+    sys.exit('invalid experiment type')
 
 if args.loo != None:
-    if args.loo< nsubj_group
-        align_data_group1_loo = np.delete(align_data_group1, args.loo,2)
-        align_data_group2_loo = align_data_group2
-    else
-        align_data_group1_loo = align_data_group1
-        align_data_group2_loo = np.delete(align_data_group2, args.loo-nsubj_group,2)
-
-(nvoxel_align, nTR_align, nsubjs_align) = align_data.shape
-(nvoxel_pred , nTR_pred , nsubjs_pred)  = pred_data.shape
-
-# make sure the dimension of dataset is consistent with input args
-assert nvoxel_pred == nvoxel_align
-assert nvoxel_pred == args.nvoxel
+    align_data_group1_loo = np.delete(align_data_group1, args.loo,2)
+    align_data_group2_loo = np.delete(align_data_group2, args.loo,2)
 
 # run alignment
 print 'start alignment'
 if args.align_algo != 'noalign':
     algo = importlib.import_module('alignment_algo.'+args.align_algo)
+if not os.path.exists(options['working_path']+'group1/'):
+    os.mkdir(options['working_path']+'group1/')
+if not os.path.exists(options['working_path']+'group2/'):
+    os.mkdir(options['working_path']+'group2/')
+
+
+options_group1 = copy.deepcopy(options)
+options_group1['working_path'] = options['working_path']+'group1/'
+options_group2 = copy.deepcopy(options)
+options_group2['working_path'] = options['working_path']+'group2/'
 
 for i in range(args.niter):
-
     if args.align_algo != 'noalign':
-        options['working_path'] += 'group1/'
-        new_niter = algo.align(align_data_group1_loo, options, args, '')
-        workspace_group1 = np.load(options['working_path']+args.align_algo+'__'+str(new_niter)+'.npz')
+
+        print '\nalign group1'
+        new_niter = algo.align(align_data_group1_loo, options_group1, args, '')
+        workspace_group1 = np.load(options_group1['working_path']+args.align_algo+'__'+str(new_niter)+'.npz')
         S_group1 = workspace_group1['G'].T
+        W_group1 = workspace_group1['R']
+        #S_group1 = stats.zscore(workspace_group1['G'], axis=0, ddof=1).T
         workspace_group1.close()
 
-        options['working_path'].replace('group1/','group2/')
-        new_niter = algo.align(align_data_group2_loo, options, args, '')
-        workspace_group2 = np.load(options['working_path']+args.align_algo+'__'+str(new_niter)+'.npz')
+        print '\nalign group2'
+        new_niter = algo.align(align_data_group2_loo, options_group2, args, '')
+        workspace_group2 = np.load(options_group2['working_path']+args.align_algo+'__'+str(new_niter)+'.npz')
         S_group2 = workspace_group2['G'].T
+        W_group2 = workspace_group2['R']
+        #S_group2 = stats.zscore(workspace_group2['G'],axis=0, ddof=1).T
         workspace_group2.close()
 
-        options['working_path'].replace('group2/','')
+    if args.exptype == 'idvclas':
+        accu = []
+        for label, pred_data in enumerate([pred_data_group1, pred_data_group2]):
+            Am = pred_data.dot(S_group1.T)
+            pert = np.zeros((Am.shape))
+            np.fill_diagonal(pert,1)
 
-    Am = pred_data.dot(S_group1)
-    pert = np.zeros((Am.shape))
-    np.fill_diagonal(pert,1)
-    U1, _, V1 = np.linalg.svd(Am+0.001*pert,full_matrices=False)
+            U1, _, V1 = np.linalg.svd(Am+0.00*pert,full_matrices=False)
+            Am = pred_data.dot(S_group2.T)
+            U2, _, V2 = np.linalg.svd(Am+0.00*pert,full_matrices=False)
 
-    Am = pred_data.dot(S_group2)
-    U2, _, V2 = np.linalg.svd(Am+0.001*pert,full_matrices=False)
+            W1 = U1.dot(V1)
+            W2 = U2.dot(V2)
 
-    W1 = U1.dot(V1)
-    W2 = U2.dot(V2)
+            err1 = np.linalg.norm(pred_data-W1.dot(S_group1),'fro')
+            err2 = np.linalg.norm(pred_data-W2.dot(S_group2),'fro')
+            if label == 0:
+                print '\nerr1:{:.3f}, err2:{:.3f}, err2-err1:{}'.format(err1,err2,err2-err1),
+            else:
+                print 'err1:{:.3f}, err2:{:.3f}, err1-err2:{}'.format(err1,err2,err1-err2),
 
-    err1 = np.norm(pred_data-W1.dot(S_group1),'fro')
-    err2 = np.norm(pred_data-W2.dot(S_group2),'fro')
+            if err2 - err1 > 0:
+                accu.append(int(label == 0))
+            elif err1 - err2 > 0:
+                accu.append(int(label == 1))
+            print accu
+    elif args.exptype == 'idvclas_svm':
+        dim = args.nvoxel*args.nTR
+        trn_data  = np.zeros((nsubj_group*2-2, dim))
+        tst_data  = np.zeros((2, dim))
 
-    if   err1 < err2 and args.loo < 10:
-        accu = 1
-    elif err1 < err2 and args.loo > 10:
-        accu = 0
-    elif err1 > err2 and args.loo < 10:
-        accu = 0
-    elif err1 > err2 and args.loo > 10:
-        accu = 1
+        trn_label = np.zeros(nsubj_group*2-2)
+        tst_label = np.zeros(2)
+
+        for i in xrange(W_group1.shape[2]):
+            tmp1 = W_group1[:, :, i].dot(S_group1)
+            trn_data[i,:] = tmp1.reshape(1, dim)
+            trn_label[i] = 0
+            tmp2 = W_group2[:, :, i].dot(S_group2)
+            trn_data[i+nsubj_group-1,:] = tmp2.reshape(1, dim)
+            trn_label[i+nsubj_group-1] = 1
+
+        tst_data[0, :] = pred_data_group1.reshape(1, dim)
+        tst_data[1, :] = pred_data_group2.reshape(1, dim)
+        tst_label[0] = 0
+        tst_label[1] = 1
+
+        clf = NuSVC(nu=0.5, kernel = 'linear')
+        clf.fit(trn_data, trn_label)
+        pred_label = clf.predict(tst_data)
+        print pred_label
+
+        accu = sum(pred_label == tst_label)/float(len(pred_label))
 
     np.savez_compressed(options['working_path']+args.align_algo+'_acc_'+str(new_niter)+'.npz',accu = accu)
     print np.mean(accu)
